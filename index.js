17a2b033df66aaa2639e6fc4223095347775c68e
Biswajit Paul <Biswajit.Paul@3meadows.com>
2023-07-07 12:59:20 +0530

    Add git hooks

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const _filename = path.join(__dirname, '/worker/index.js');
 
-                        const worker = new Worker(_filename);
+const worker = new Worker(_filename);
 
-                      worker.once('message', (message) => {
-                        console.log(message);  // Prints 'Hello, world!'.
-                      });
+worker.once('message', (message) => {
+  console.log(message);  // Prints 'Hello, world!'.
+});
 
-                                        worker.postMessage('Hello, world!');
+worker.postMessage('Hello, world!');
